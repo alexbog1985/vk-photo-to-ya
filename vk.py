e7d3638debec8eeec4ce80ca2514d44ea746ef9c
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2024-02-19 14:19:09 +0300

    add get_photo_albums

--- vk.py
+++ vk.py
@@ -1,7 +1,6 @@
 import secret
 import requests
 
-
 VK_TOKEN = secret.VK_TOKEN
 
 
@@ -25,26 +24,45 @@ class VKAPIClient:
     def _build_url(self, api_method):
         return f'{self.API_BASE_URL}/{api_method}'
 
-    def get_user(self):
-        params = self._get_common_params()
-        params.update({'user_id': self.user_id})
-        response = requests.get(self._build_url('users.get'), params=params)
+    def _check_error(self, response):
         if 200 <= response.status_code < 300:
             if response.json().get('response'):
-                result = response.json()['response'][0]
-                self.user_int_id = result['id']
-                self.user_name = result['first_name'] + ' ' + result['last_name']
-                return
+                return response.json()['response']
             elif response.json().get('error'):
                 error_dict = response.json()['error']
                 error_code = error_dict.get('error_code')
                 error_msg = error_dict.get('error_msg')
-                print(f'Ошибка: №{error_code} - {error_msg}')
+                print(f'Ошибка №{error_code} - {error_msg}')
             else:
-                return print(f'Пользователя с id: {self.user_id} не существует')
-
+                return response.json()
         else:
-            return print("Ошибка соединения с сервером:", response.status_code)
+            print("Ошибка соединения с сервером:", response.status_code)
+
+    def get_user(self):
+        params = self._get_common_params()
+        params.update({'user_id': self.user_id})
+        response = requests.get(self._build_url('users.get'), params=params)
+        if self._check_error(response):
+            result = self._check_error(response)
+            self.user_int_id = result[0]['id']
+            self.user_name = result[0]['first_name'] + ' ' + result[0]['last_name']
+
+    def get_photo_albums(self):
+        params = self._get_common_params()
+        params.update({
+            'owner_id': self.user_int_id,
+        })
+        response = requests.get(self._build_url('photos.getAlbums'), params=params)
+        if self._check_error(response):
+            albums = [{'id': 'profile', 'title': 'Фотографии профиля'},
+                      {'id': 'wall', 'title': 'Фотографии со стены'},
+                      {'id': 'saved', 'title': 'Сохраненные фотографии'},
+                      ] + ([item for item in self._check_error(response)['items']])
+            result = []
+            for num, album in enumerate(albums):
+                result.append([num, {album['id']: album['title']}])
+            print(result)
+            return result
 
 
 if __name__ == '__main__':
@@ -52,4 +70,5 @@ if __name__ == '__main__':
     print(
         vk.user_name,
         vk.user_int_id,
+        vk.get_photo_albums()
     )
