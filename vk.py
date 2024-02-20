010e5a8b7bb080a2df01a2c410b55def29b944b6
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2024-02-20 16:47:14 +0300

    get photos from albums in vk.py

--- vk.py
+++ vk.py
@@ -8,7 +8,7 @@ VK_TOKEN = secret.VK_TOKEN
 class VKAPIClient:
     API_BASE_URL = 'https://api.vk.com/method'
 
-    def __init__(self, user_id, token=VK_TOKEN):
+    def __init__(self, user_id=None, token=VK_TOKEN):
         self.albums = None
         self.user_int_id = None
         self.user_name = None
@@ -39,40 +39,57 @@ class VKAPIClient:
             print(f'Пользователь с ID: "{self.user_id}" не найден')
 
     def get_photo_albums(self):
+        print('Получаем список фотоальбомов пользователя')
         params = self._get_common_params()
         params.update({
             'owner_id': self.user_int_id,
         })
         response = requests.get(self._build_url('photos.getAlbums'), params=params)
-        if self.user_name and _check_error(response):
-            albums = [{'id': 'profile', 'title': 'Фотографии профиля'},
-                      {'id': 'wall', 'title': 'Фотографии со стены'},
-                      {'id': 'saved', 'title': 'Сохраненные фотографии'},
-                      ] + ([item for item in _check_error(response)['items']])
-            result = []
+        result = []
+        if 'error' not in _check_error(response).keys():
+            print('Вам доступны альбомы:')
+            albums = [
+                         {'id': 'profile', 'title': 'Фотографии профиля'},
+                         {'id': 'wall', 'title': 'Фотографии со стены'},
+                         {'id': 'saved', 'title': 'Сохраненные фотографии'},
+                     ] + [item for item in _check_error(response)['items']]
             for num, album in enumerate(albums):
                 result.append([num, {album['id']: album['title']}])
             self.albums = result
-            return result
+        else:
+            print('Вам доступны альбомы: ')
+            result = [[
+                0,
+                {'profile': 'Фотографии профиля'}
+            ]]
+            self.albums = result
+        return result
 
     def get_photos(self, album_id=0):
+        print('Получаем список фотографий альбома пользователя:')
         params = self._get_common_params()
         params.update({
             'owner_id': self.user_int_id,
-            'album_id': ''.join([str(key) for key in vk.albums[album_id][1].keys()]),
             'extended': 1,
         })
+        if list is type(self.albums):
+            params.update({'album_id': ''.join([str(key) for key in self.albums[album_id][1].keys()])})
+            album_title = ''.join([str(value) for value in self.albums[album_id][1].values()])
+        else:
+            params.update({'album_id': 'profile'})
+            album_title = 'Фотографии профиля'
         response = requests.get(self._build_url('photos.get'), params=params)
         if self.user_name and _check_error(response):
             photos = []
-            for photo in _check_error(response)['items']:
+            for photo in _check_error(response).get('items'):
                 photos.append({
                     'likes': photo['likes']['count'],
                     'create_date': datetime.datetime.fromtimestamp(photo['date']).strftime('%d-%m-%Y'),
                     'url': photo['sizes'][-1]['url'],
                     'size': photo['sizes'][-1]['type']
                 })
-            print(photos)
+            print(f'В альбоме "{album_title}" найдено {len(photos)} фотографий')
+            return photos
 
 
 def _check_error(response):
@@ -84,16 +101,19 @@ def _check_error(response):
             error_code = error_dict.get('error_code')
             error_msg = error_dict.get('error_msg')
             print(f'Ошибка №{error_code} - {error_msg}')
+            return {'error': error_code}
     else:
         print("Ошибка соединения с сервером:", response.status_code)
 
 
 if __name__ == '__main__':
-    vk = VKAPIClient('60453017')
+    vk = VKAPIClient(
+          # '60453017'
+    )
     vk.get_photo_albums()
     print(
         vk.user_name, '\n',
         vk.user_int_id, '\n',
-        vk.albums, '\n',
+        # vk.albums, '\n',
         vk.get_photos()
     )
